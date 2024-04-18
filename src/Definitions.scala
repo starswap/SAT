d435fa10bb7a4ae70e1e5563f89a936e80e69c74
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-04-18 18:14:59 +0100

    Improve toString to make it valid code

--- src/Definitions.scala
+++ src/Definitions.scala
@@ -2,7 +2,7 @@ package SAT
 
 case class Literal(polarity: Boolean, a: Atom) {
     def unary_! = Literal(!polarity, a)
-    override def toString(): String = if !polarity then s"¬{a}" else a
+    override def toString(): String = if polarity then s"+\"${a}\"" else s"!\"${a}\""
 }
 
 type Atom = String
